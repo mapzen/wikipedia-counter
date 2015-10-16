5e3869ff63dd79eb472b8187c55d49d1a4d9694c
Julian Simioni <julian@simioni.org>
2015-10-16 11:43:05 -0400

    Check existing keys, handle errors

--- index.js
+++ index.js
@@ -15,30 +15,52 @@ var i = 0;
 var en = 0;
 
 function countRequests(url) {
-  var rl = require('readline').createInterface({
-   input: request(url).pipe(zlib.createGunzip())
-  });
-
-  rl.on('line', function (line) {
-    var parts = line.split(' ');
-
-    var lang = parts[0];
-    var name = parts[1];
-    var key = lang + ":::" + name;
-    var views = parts[2];
-    i++;
-
-    if (lang !== 'en') {
+  client.exists(url, function(err, key_exists) {
+    if (key_exists) {
+      console.log("skipping " + url);
       return;
     }
-    en++;
+    console.log("counting " + url);
 
-    client.incrby(key, parseInt(views));
+    var req = request(url);
+    var zpipe = zlib.createGunzip();
 
-  });
-  rl.on('close', function() {
-    console.log(url + ": " + i + " (" + en + " en)");
-    process.exit(0);
+    var rl = require('readline').createInterface({
+      input: req.pipe(zpipe)
+    });
+
+    req.on('error', function(error) {
+      console.log(error);
+      process.exit(1);
+    });
+
+    zpipe.on('error', function(error) {
+      console.log(url + "\n" + error);
+      process.exit(1);
+    });
+
+    rl.on('line', function (line) {
+      var parts = line.split(' ');
+
+      var lang = parts[0];
+      var name = parts[1];
+      var key = lang + ":::" + name;
+      var views = parts[2];
+      i++;
+
+      if (lang !== 'en') {
+        return;
+      }
+      en++;
+
+      client.incrby(key, parseInt(views));
+    });
+
+    rl.on('close', function() {
+      console.log(url + ": " + i + " (" + en + " en)");
+      client.set(url, "ok");
+      process.exit(0);
+    });
   });
 }
 
