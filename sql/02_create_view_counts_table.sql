3843a21986885a39ee30581d5553f923b92adead
Julian Simioni <julian@simioni.org>
2015-10-22 19:49:36 -0400

    Massively optimize the view_counts table indices
    Turns out many were not required, and one should be added only after
    import.
    
    (I had over 100GB of just indices with the previous settings!)

--- sql/02_create_view_counts_table.sql
+++ sql/02_create_view_counts_table.sql
@@ -4,50 +4,8 @@
 
 CREATE TABLE view_counts
 (
-  id serial NOT NULL,
   path text,
-  language character(10),
-  count bigint,
-  CONSTRAINT view_counts_pkey PRIMARY KEY (id),
-  UNIQUE (language, path)
-)
-WITH (
-  OIDS=FALSE
+  language character(15),
+  count integer,
+  UNIQUE (path, language)
 );
-
--- Index: view_counts_count
-
--- DROP INDEX view_counts_count;
-
-CREATE INDEX view_counts_count
-  ON view_counts
-  USING btree
-  (count DESC);
-
--- Index: view_counts_id
-
--- DROP INDEX view_counts_id;
-
-CREATE INDEX view_counts_id
-  ON view_counts
-  USING btree
-  (id);
-
--- Index: view_counts_path_hash
-
--- DROP INDEX view_counts_path_hash;
-
-CREATE INDEX view_counts_path_hash
-  ON view_counts
-  USING hash
-  (path COLLATE pg_catalog."default");
-
--- Index: view_counts_path_sorted
-
--- DROP INDEX view_counts_path_sorted;
-
-CREATE INDEX view_counts_path_sorted
-  ON view_counts
-  USING btree
-  (path COLLATE pg_catalog."default" DESC);
-
