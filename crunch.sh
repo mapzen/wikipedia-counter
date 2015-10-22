492f4b56be31fc1b07a6e1d84178739b484a35f9
Julian Simioni <julian@simioni.org>
2015-10-22 19:52:14 -0400

    Improve main data import script

--- crunch.sh
+++ crunch.sh
@@ -14,10 +14,14 @@ psql -d wikipedia < sql/01_modify_wiki_geo_table.sql
 
 # in parallel import view counts data
 # a.) from web (easier on disk, slower)
-# cat pagecounts.txt | xargs -P 8 -n 1 node index.js
-# b.) from files ( faster for multiple runs)
+# cat pagecounts.txt | time xargs -P 4 -n 1 nice time node index.js
+# b.) from files (faster for multiple runs)
 # wget -I pagecounts.txt # use this to download
-find /home/julian/data/wikipedia/*.gz | xargs -P 8 -n 1 node index.js
+time find /home/julian/data/wikipedia/*.gz | xargs -P 4 -n 1 nice time node index.js
+
+# add additional index for count
+# (adding indicies after import is faster - http://www.postgresql.org/docs/9.4/static/populate.html )
+psql -d wikipedia < sql/02.5_add_count_index.sql
 
 # run SQL query
 # a.) with result for each lat/lon
---
+++ sql/02.5_add_count_index.sql
@@ -0,0 +1,8 @@
+-- Index: view_counts_count
+
+-- DROP INDEX view_counts_count;
+
+CREATE INDEX view_counts_count
+  ON view_counts
+  USING btree
+  (count DESC);
